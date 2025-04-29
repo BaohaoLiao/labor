603984aef3957be6d5dbf8353b516d71109103ea
baohaoliao <liaobaohao@gmail.com>
2025-04-30 00:50:14 +0200

    qwen3 template

--- utils/data.py
+++ utils/data.py
@@ -16,9 +16,17 @@ PROMPT_TEMPLATES = {
         "'ANSWER: $LETTER' (without quotes) where LETTER is one of ABCD. "
         "Think step by step before answering.\n\n{input}<｜Assistant｜><think>\n"
     ),
-    "qwen25-math-cot": (
+    "qwen3-think": (
         "<|im_start|>system\nPlease reason step by step, and put your final answer within \\boxed{{}}.<|im_end|>\n"
         "<|im_start|>user\n{input}<|im_end|>\n"
+        "<|im_start|>assistant\n<think>\n"
+    ),
+    "qwen3-think-choice": (
+        "<|im_start|>user\nAnswer the following multiple choice question. "
+        "The last line of your response should be of the following format: "
+        "'ANSWER: $LETTER' (without quotes) where LETTER is one of ABCD. "
+        "Think step by step before answering.\n\n"
+        "{input}<|im_end|>\n"
         "<|im_start|>assistant\n<think>\n"
     ),
 }
